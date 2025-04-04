0b8ceb0d50cbc4653d7b61fe605fbe09b449cc8d
Daniel <89912387+dlsns@users.noreply.github.com>
2025-04-04 09:28:28 +0200

    test ocr for pdf conversion

--- backend/pdf-to-latex-test/pdf-to-latex.py
+++ backend/pdf-to-latex-test/pdf-to-latex.py
@@ -2,7 +2,7 @@ import aspose.pdf as ap
 
 input_pdf = "backend/BeispielPruefung2024.pdf"
 
-output_tex = "backend/BeispielPruefung2024Gen.tex"
+output_tex = "backend/pdf-to-latex-test/BeispielPruefung2024Gen.tex"
 
 document = ap.Document(input_pdf)
 
