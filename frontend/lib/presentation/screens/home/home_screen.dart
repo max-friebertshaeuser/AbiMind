01d72a0760e60d10e5d468b50b690ff9ab2ba486
maxfriebertshauser <22frma1bif@hft-stuttgart.de>
2025-04-15 08:35:16 +0200

    feat: add statistics button to home screen for navigation to stats page

--- frontend/lib/presentation/screens/home/home_screen.dart
+++ frontend/lib/presentation/screens/home/home_screen.dart
@@ -37,6 +37,14 @@ class HomeScreen extends StatelessWidget {
               icon: const Icon(Icons.logout), label: const Text('Log out'),
             ),
             ElevatedButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, AppRoutes.start);
+              },
+              label: const Text('Statistics'),
+              icon: const Icon(Icons.line_axis_outlined),
+            ),
+            ),
+            ElevatedButton.icon(
               onPressed: () async {
                 print(await FirebaseService().getExam('OHLESyc19sRHmLTVOUji'));
               },
