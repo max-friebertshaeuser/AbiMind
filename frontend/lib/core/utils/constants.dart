a637931f5aca9c526324989f3d472f9a60e58612
maxfriebertshauser <22frma1bif@hft-stuttgart.de>
2025-04-16 21:40:52 +0200

    feat: update start screen layout with new structure and example exercise data

--- frontend/lib/core/utils/constants.dart
+++ frontend/lib/core/utils/constants.dart
@@ -7,6 +7,18 @@ const kAchievdHeaderStyle = TextStyle(
 const kHeaderStyle = TextStyle(
     fontSize: 32, fontFamily: 'Montserrat');
 
+const texamleExeciese ={
+  'id': 'example',
+  'name': 'example',
+  'description': 'example description',
+  'type': 'mathe',
+  'category': 'strength',
+  'level': 'beginner',
+  'duration': 30,
+  'reps': 10,
+  'sets': 3,
+};
+
 
 const String kSampleText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
     'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
