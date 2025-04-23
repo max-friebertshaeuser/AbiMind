9699f34f34b7fc5003f5c7cc0b125cd0ca8c7677
maxfriebertshauser <22frma1bif@hft-stuttgart.de>
2025-04-23 16:20:57 +0200

    feat: refactor text styles and improve layout consistency in constants and start screen

--- frontend/lib/core/utils/constants.dart
+++ frontend/lib/core/utils/constants.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
-
 const kAchievdHeaderStyle = TextStyle(
-    fontSize: 24, fontWeight: FontWeight.bold, fontFamily: 'Montserrat');
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+  fontFamily: 'Montserrat',
+);
 
-const kHeaderStyle = TextStyle(
-    fontSize: 24, fontFamily: 'Montserrat');
+const kHeaderStyle = TextStyle(fontSize: 24, fontFamily: 'Montserrat');
 
 const percentageStyle = TextStyle(
   fontSize: 32,
@@ -13,10 +14,10 @@ const percentageStyle = TextStyle(
   color: Color(0xFF584178),
 );
 
-const smallTextStyle = TextStyle(
-  fontSize: 11,
-  color: Color(0xFF584178)
-);
+const smallTextStyle = TextStyle(fontSize: 11, color: Color(0xFF584178));
+const lableTextStyle = TextStyle(color: Colors.white, fontSize: 12);
+
+const middleTextStyle = TextStyle(fontSize: 15, color: Color(0xFF584178));
 
 const texamleExeciese = {
   'id': 'example',
@@ -34,81 +35,35 @@ const statistic = {
   'solvedPercentageNumeric': 0.65,
   'solvedPercentage': 65,
   'examParts': {
-    {
-      'name': 'Analysis',
-      'done': 10,
-      'total': 10,
-    },
-    {
-      'name': 'Stochastik',
-      'done': 5,
-      'total': 10,
-    },
-    {
-      'name': 'Geometrie',
-      'done': 10,
-      'total': 10,
-    },
-
-  }
+    {'name': 'Analysis', 'done': 10, 'total': 10},
+    {'name': 'Stochastik', 'done': 5, 'total': 10},
+    {'name': 'Geometrie', 'done': 10, 'total': 10},
+  },
 };
 
 const streak = {
   'streak': 5,
   'goal': 90.0,
   'tenDayLearnProgress': {
-    {
-      'day': '2023-10-01',
-      'minutes': 100,
-    },
-    {
-      'day': '2023-10-02',
-      'minutes': 120,
-    },
-    {
-      'day': '2023-10-03',
-      'minutes': 95,
-    },
-    {
-      'day': '2023-10-04',
-      'minutes': 105,
-    },
-    {
-      'day': '2023-10-05',
-      'minutes': 109,
-    },
-    {
-      'day': '2023-10-06',
-      'minutes': 95,
-    },
-    {
-      'day': '2023-10-07',
-      'minutes': 100,
-    },
-    {
-      'day': '2023-10-08',
-      'minutes': 120,
-    },
-    {
-      'day': '2023-10-09',
-      'minutes': 100,
-    },
-    {
-      'day': '2023-10-10',
-      'minutes': 100,
-    },
-
-
-
-  }
+    {'day': '2023-10-01', 'minutes': 100},
+    {'day': '2023-10-02', 'minutes': 120},
+    {'day': '2023-10-03', 'minutes': 95},
+    {'day': '2023-10-04', 'minutes': 105},
+    {'day': '2023-10-05', 'minutes': 109},
+    {'day': '2023-10-06', 'minutes': 95},
+    {'day': '2023-10-07', 'minutes': 100},
+    {'day': '2023-10-08', 'minutes': 120},
+    {'day': '2023-10-09', 'minutes': 100},
+    {'day': '2023-10-10', 'minutes': 100},
+  },
 };
 
 final tenDayLearnProgress = (streak['tenDayLearnProgress'] as Set).toList();
-final minutesList = tenDayLearnProgress
-    .map((entry) => entry['minutes'] as int)
-    .toList();
+final minutesList =
+    tenDayLearnProgress.map((entry) => entry['minutes'] as int).toList();
 final maxMinutes = minutesList.reduce((a, b) => a > b ? a : b);
 final minMinutes = minutesList.reduce((a, b) => a < b ? a : b);
+
 
 
 const String kSampleText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
