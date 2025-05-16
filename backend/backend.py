df934bb727cc6196b6bf2e3994ef48a7e564285e
Daniel <89912387+dlsns@users.noreply.github.com>
2025-05-16 11:09:35 +0200

    get student answer from database

--- backend/backend.py
+++ backend/backend.py
@@ -1,14 +1,19 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, BackgroundTasks
 from pydantic import BaseModel
+from gptprompt.prompter import correctExercice
 
 app = FastAPI()
 
-class Correction_Request(BaseModel):#
+class Correction_Request(BaseModel):
     userID: str
     examID: str
+    exerciseID: str
 
-@app.post("/corretion/")
-async def correct_exam(request: Correction_Request):
-    return {
+@app.post("/correction/")
+async def correct_exam(request: Correction_Request, background_tasks: BackgroundTasks):
+        background_tasks.add_task(correctExercice, request.userID, request.examID, request.exerciseID)    
+        return {
         "message": f"Exam '{request.examID}' is being corrected for user '{request.userID}'.",
     }
+
+# starten mit: "python -m uvicorn backend:app --reload"
